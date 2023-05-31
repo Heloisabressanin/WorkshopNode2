1b01ebd2e89fcd1da213e6ae1469ca10f2ab195a
Heloisabressanin <114985653+Heloisabressanin@users.noreply.github.com>
2023-05-31 11:24:45 +0200

    logout

--- Apps/Client/src/components/Navlinks.tsx
+++ Apps/Client/src/components/Navlinks.tsx
@@ -6,34 +6,39 @@ import "../css/Pages.css";
 
 
 const NavLinksComponent = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+   const handleLogout = () => {
+     localStorage.removeItem("token");
+     localStorage.removeItem("loggedIn");
 
+     setIsLoggedIn(false);
+    window.location.href = "/"; 
+   };
+ const token = localStorage.getItem("token");
+ const loggedIn = localStorage.getItem("loggedIn");
+ if (token && loggedIn && !isLoggedIn) {
+   setIsLoggedIn(true);
+ }
     return (
-        <div className="nav-links">
-            <div className="nav-left">
-Test
+      <div className="nav-links">
+        <div className="nav-left">Test</div>
+        <div className="nav-right">
+          <>
+            <div className="link-nav">
+              <Link to="/" className="hover-link">
+                Home
+              </Link>
             </div>
-            <div className="nav-right">
-
-                <>
-
-                
-                    <div className="link-nav">
-                        <Link to="/" className="hover-link">
-                            Home
-                        </Link>
-                    </div>
-                    <div className="link-nav">
-                        <Link to="/register" className="hover-link">
-                            Register
-                        </Link>
-                    </div>
-
-                </>
-
-
+            <div className="link-nav">
+              <Link to="/register" className="hover-link">
+                Register
+              </Link>
             </div>
+          </>
         </div>
+        {isLoggedIn && <button className="btn-logout" onClick={handleLogout}>Logout</button>}
+      </div>
     );
 };
 
